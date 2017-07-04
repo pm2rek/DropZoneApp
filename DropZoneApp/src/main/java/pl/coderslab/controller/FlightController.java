abca104204fd29204faef8039cc4e2cafc2315d5
Marek Piątek <pm2rek@gmail.com>
2017-07-04 22:21:41 +0200

    Entity Flight edit and delete functions add

--- DropZoneApp/src/main/java/pl/coderslab/controller/FlightController.java
+++ DropZoneApp/src/main/java/pl/coderslab/controller/FlightController.java
@@ -1,12 +1,11 @@
 package pl.coderslab.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -39,11 +38,41 @@ public class FlightController {
 	}
 	
 	@RequestMapping(path = "/add", method = RequestMethod.POST)
-	public String processAddFlight(@Valid @ModelAttribute Flight flight, BindingResult result, Model model) {
+	public String processAddFlight(@ModelAttribute Flight flight, BindingResult result) {
 		if (result.hasErrors()) {
 			return "flightForm";
 		}
 		flightRepository.save(flight);
 		return "redirect:/flights/list";
 	}
+	
+	// EDIT
+		@RequestMapping(path = "/edit/{id}", method = RequestMethod.GET)
+		public String editFlight(Model model, @PathVariable Long id) {
+			model.addAttribute("flight", flightRepository.findOne(id));
+			model.addAttribute("aircrafts", aircraftRepository.findAll());
+			return "flightForm";
+		}
+
+		@RequestMapping(path = "/edit/{id}", method = RequestMethod.POST)
+		public String processEditFlight(@ModelAttribute Flight flight, BindingResult result) {
+			if (result.hasErrors()) {
+				return "flightForm";
+			}
+			flightRepository.save(flight);
+			return "redirect:/flights/list";
+		}
+
+		// DELETE
+		@RequestMapping(path = "/delete/{id}", method = RequestMethod.GET)
+		public String removeFlight(@PathVariable Long id, Model model) {
+			model.addAttribute("object", flightRepository.findOne(id));
+			return "confirmRemove";
+		}
+
+		@RequestMapping(path = "/delete/{id}", method = RequestMethod.POST)
+		public String processRemoveFlight(@ModelAttribute Flight flight) {
+			flightRepository.delete(flight);
+			return "redirect:/flights/list";
+		}
 }
